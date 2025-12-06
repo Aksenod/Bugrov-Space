b1bdc16799f927f53bab66d6403ee7ee4be3ea62
Aksenod <Aksenod@gmail.com>
2025-12-06 13:46:56 +0300

    Updates

---
+++ assets/eye-DgwA3vBE.js
@@ -0,0 +1,1 @@
+import{c}from"./index-C_nb8_l2.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=c("arrow-left",e);const o=[["path",{d:"M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0",key:"1nclc0"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]],a=c("eye",o);export{r as A,a as E};
