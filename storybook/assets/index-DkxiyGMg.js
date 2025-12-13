65b63ff7b5aefc30804a02a3ba1a1d6482fe1e99
Aksenod <Aksenod@gmail.com>
2025-12-13 16:18:07 +0300

    Updates

--- storybook/assets/index-DkxiyGMg.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-5iRuQup1.js";import{r as o}from"./index-CV8vm00V.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ storybook/assets/index-W2zGFuCn.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Do9DfNg2.js";import{r as o}from"./index-Bj10mI2F.js";var t=o();const m=r(t);export{m as R,t as r};
