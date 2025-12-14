eb07deaf1a0d42b7e36160f945c5dbeb45458500
Aksenod <Aksenod@gmail.com>
2025-12-14 11:46:06 +0300

    Updates

---
+++ storybook/assets/formatter-QJ4M4OGQ-BOZnMhKa.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CaXCbVVI.js";import{I as t}from"./iframe-DFO4omvp.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-CQeT-GoT.js";import"./index-DweCD_Or.js";import"./index-Ag2XV737.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
